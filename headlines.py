0c014d65f8f4c9086df2cbad8999cd77e55acc75
Oscar Cervantes <oscar@oc8474400218.ibm.com>
2016-08-23 18:06:11 -0500

    templates

--- headlines.py
+++ headlines.py
@@ -2,6 +2,8 @@
 
 import feedparser
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 BBC_FEED = "http://feeds.bbci.co.uk/news/rss.xml"
@@ -27,14 +29,16 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 def get_news(publication="bbc"):
     feed = feedparser.parse(RSS_FEEDS[publication])
     first_article = feed['entries'][0]
-    return """<html>
-        <body>
-            <h1> Headlines </h1>
-            <b>{0}</b> <br/>
-            <i>{1}</i> <br/>
-            <p>{2}</p> <br/>
-        </body>
-        </html>""".format(first_article.get("title"), first_article.get("published"), first_article.get("summary"))
+    return render_template("home.html",title=first_article.get("title"),published=first_article.get("published"),summary=first_article.get("summary"))
+
+    #return """<html>
+    #    <body>
+    #        <h1> Headlines </h1>
+    #        <b>{0}</b> <br/>
+    #        <i>{1}</i> <br/>
+    #        <p>{2}</p> <br/>
+    #    </body>
+    #    </html>""".format(first_article.get("title"), first_article.get("published"), first_article.get("summary"))
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
